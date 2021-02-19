9f26ecbfccf5602dcb3cddce2fdfdd9a398a1cc4
MaximChuikov <71207704+MaximChuikov@users.noreply.github.com>
2021-02-19 19:05:43 +0500

    First release

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using FunctionBuilder.Logic;
+
+namespace Fun
+{
+    
+    class Program
+    {
+        static void Main()
+        {
+            var f = new Function("13,2-1");
+            Console.WriteLine($"ОПЗ = {f.ReversePolishNotation}, решение = {f.Answer}");
+            Console.ReadKey();
+        }
+    }
+}
